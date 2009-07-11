735871d95d0fcb82d22f116d6980b5e78aee8934
Clint Branham <github@goodolclint.me>
2009-07-11 22:21:50 +0000

    Dictionaries should now sort correctly Bibles should now be searchable

--- e-Sword9Converter/clsThreadSafeCollection.cs
+++ e-Sword9Converter/clsThreadSafeCollection.cs
@@ -226,5 +226,13 @@ namespace eSword9Converter
         }
 
         #endregion
+
+        public void FromArray(T[] array)
+        {
+            foreach (T item in array)
+            {
+                this.Add(item);
+            }
+        }
     }
 }
